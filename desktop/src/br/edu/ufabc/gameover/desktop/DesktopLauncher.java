1c3d80b25a6801c191e398f82550aaee8bac65c5
Vitor Baggi de Almeida <vitor_baggi@hotmail.com>
2019-03-12 19:36:34 -0300

    commit inicial

---
+++ desktop/src/br/edu/ufabc/gameover/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package br.edu.ufabc.gameover.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import br.edu.ufabc.gameover.GameOverMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameOverMain(), config);
+	}
+}
